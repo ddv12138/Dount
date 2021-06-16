e36f07236e6ec657c80f1eb922080488fd26eb8a
ddv12138 <644077730@qq.com>
2021-06-16 23:17:11 +0800

    光照计算

--- dount.py
+++ dount.py
@@ -8,7 +8,7 @@ r = 3
 a, b = (7.0, 0.0)
 
 # 光源
-# ax.scatter(0, 30, -10)
+light = [-2,-2,-4]
 
 # 画初始圆
 degree_rotate = math.pi/8
@@ -16,12 +16,6 @@ theta = np.arange(0, math.pi*2, degree_rotate)
 x = a + r * np.cos(theta)
 y = b + r * np.sin(theta)
 z = 0.0
-
-
-coodX = np.array([])
-coodY = np.array([])
-coodZ = np.array([])
-
 
 def rotate_x(x, y, z, degree):
     return [x, y*math.cos(degree)-z*math.sin(degree), y * math.sin(degree) + z * math.cos(degree)]
@@ -34,41 +28,56 @@ def rotate_y(x, y, z, degree):
 def rotate_z(x, y, z, degree):
     return [x * math.cos(degree) - y * math.sin(degree), x * math.sin(degree) + y * math.cos(degree), z]
 
+degree_rotate = math.pi/18
 
-def flat(x, y, z):
-    newZ = []
-    for _ in x:
-        newZ.append(20)
-    return [x, y, newZ]
+coodX = np.array([])
+coodY = np.array([])
+coodZ = np.array([])
 
+oCoodX = np.array([])
+oCoodY = np.array([])
+oCoodZ = np.array([])
+
+for i in np.arange(0, math.pi*2, degree_rotate):
+
+    cood = rotate_y(x, y, z, degree=i)
+    coodX = np.hstack((coodX, cood[0]))
+    coodY = np.hstack((coodY, cood[1]))
+    coodZ = np.hstack((coodZ, cood[2]))
+
+    oCood = rotate_y(a, b, 0, degree=i)
+    oCoodX = np.hstack((oCoodX, oCood[0]))
+    oCoodY = np.hstack((oCoodY, oCood[1]))
+    oCoodZ = np.hstack((oCoodZ, oCood[2]))
 
 offset = -1
-def flat_test(x, y, z):
+def flat_test(x, y, z,oCood):
     global offset
     width = 80
     height = 40
     matrix = [[None for i in range(width)] for i in range(height)]
 
-    # print(min(x[np.argmin(x)],y[np.argmin(y)]))
     if offset < 0:
         offset = abs(min(x[np.argmin(x)],y[np.argmin(y)]))
-    x = (x+offset+1)*1.7
-    y = (y+offset+1)*3.5
 
     for i in range(len(x)):
 
-        tmp_x = round(x[i])
-        tmp_y = round(y[i])
+        tmp_x = round((x[i]+offset+1)*1.7)
+        tmp_y = round((y[i]+offset+1)*3.5)
 
         if 0 > tmp_x >= width or 0 > tmp_y >= height:
             continue
 
         if (matrix[tmp_x][tmp_y] != None and matrix[tmp_x][tmp_y] < z[i]) or matrix[tmp_x][tmp_y] == None:
-            # dot_product = x[i]*0+y[i]*30-z[i]*10
-            # if dot_product < 0:
-            #     print(dot_product)
-            #     continue
-            matrix[tmp_x][tmp_y] = z[i]
+            # print(len(oCood[0]))
+            tmp = int(i/16)
+            # print(tmp)
+            currCircleCenter = [oCood[0][tmp],oCood[1][tmp],oCood[2][tmp]]
+            surfaceNormal = [x[i]-currCircleCenter[0],y[i]-currCircleCenter[1],z[i]-currCircleCenter[2]]
+            dotProduct = np.dot(light,surfaceNormal)
+            # print(dotProduct)
+            if dotProduct>=0:
+                matrix[tmp_x][tmp_y] = dotProduct
 
     print_matrix(matrix)
 
@@ -78,39 +87,38 @@ def print_matrix(matrix):
     for x in range(len(matrix)):
         for y in range(len(matrix[x])):
             if matrix[x][y] != None:
-                str += "."
+                # str += "."
+                index = round(matrix[x][y])
+                if index>11:
+                    index = 11
+                # print(index)
+                str += ".,-~:;=!*#$@"[index]
             else:
                 str += " "
         str += "\n"
     print(str)
 
 
-degree_rotate = math.pi/18
-
-for i in np.arange(0, math.pi*2, degree_rotate):
-
-    cood = rotate_y(x, y, z, degree=i)
-    orgin_cood = rotate_y(a, b, 0, degree=i)
-    coodX = np.hstack((coodX, cood[0]))
-    coodY = np.hstack((coodY, cood[1]))
-    coodZ = np.hstack((coodZ, cood[2]))
-
-
-def drew2():
+def drew():
     degree = math.pi/36
     newCood = rotate_z(coodX, coodY, coodZ, degree=degree)
+    newOCood = rotate_z(oCoodX, oCoodY, oCoodZ, degree=degree)
     while True:
         print("\x1b\x63", end="")
         newCood = rotate_y(newCood[0], newCood[1], newCood[2], degree=degree)
         newCood = rotate_x(newCood[0], newCood[1], newCood[2], degree=degree)
-#        newCood = rotate_z(newCood[0], newCood[1], newCood[2], degree=degree)
-        flat_test(newCood[0], newCood[1], newCood[2])
+        # newCood = rotate_z(newCood[0], newCood[1], newCood[2], degree=degree)
+
+        newOCood = rotate_y(newOCood[0], newOCood[1], newOCood[2], degree=degree)
+        newOCood = rotate_x(newOCood[0], newOCood[1], newOCood[2], degree=degree)
+        # newOCood = rotate_z(newOCood[0], newOCood[1], newOCood[2], degree=degree)
+        flat_test(newCood[0], newCood[1], newCood[2],newOCood)
         time.sleep(0.1)
 
 
 def main():
     print("\x1b\x63", end="")
-    drew2()
+    drew()
 
 
 if __name__ == '__main__':
